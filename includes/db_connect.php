bed65a43d8f3c71cde60815397f248527489dbd8
Ruhul Amin <asmruhulamin47@gmail.com>
2026-05-26 00:46:12 +0600

    Update db_connect.php port handling

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -30,8 +30,9 @@ if ($pass === false) {
     $pass = "";
 }
 $dbname = getenv('DB_NAME') ?: "uiu_scholarnet";
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
 if (!$conn) {
     http_response_code(500);
