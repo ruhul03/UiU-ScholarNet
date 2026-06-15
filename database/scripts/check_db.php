bdc159e74acd8b6ae1b02a4226412d2d8eb2af2b
Ruhul Amin <asmruhulamin47@gmail.com>
2026-06-15 21:19:58 +0600

    Cleanup: Remove obsolete scratch and database migration scripts

--- database/scripts/check_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('includes/db_connect.php');
-$res = $conn->query("DESCRIBE projects");
-while($row = $res->fetch_assoc()) {
-    echo $row['Field'] . "\n";
-}
-?>
--- database/scripts/check_notif2.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('includes/db_connect.php');
-$res = $conn->query("SHOW COLUMNS FROM notifications");
-while($row = $res->fetch_assoc()) {
-    echo $row['Field'] . " | " . $row['Type'] . " | " . $row['Null'] . " | " . $row['Default'] . "\n";
-}
-?>
